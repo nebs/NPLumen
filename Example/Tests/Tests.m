8b090094b052b0ec54dc9fe9a44fa550e123c77a
Nebojsa Petrovic <nebspetrovic@gmail.com>
2015-07-25 12:24:42 -0700

    Remove failing tests form samples

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -1,31 +1,4 @@
-//
-//  NPLumenTests.m
-//  NPLumenTests
-//
-//  Created by Nebojsa Petrovic on 07/25/2015.
-//  Copyright (c) 2015 Nebojsa Petrovic. All rights reserved.
-//
-
-// https://github.com/Specta/Specta
-
 SpecBegin(InitialSpecs)
-
-describe(@"these will fail", ^{
-
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
-    
-    it(@"will wait for 10 seconds and fail", ^{
-        waitUntil(^(DoneCallback done) {
-        
-        });
-    });
-});
 
 describe(@"these will pass", ^{
     
